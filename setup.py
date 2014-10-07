8540c99e2a11d867a129257767f19c8312c40b87
Rob Speer <rob@luminoso.com>
2014-10-06 22:09:15 -0400

    more description

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ if sys.version_info[0] >= 3:
 
 LONG_DESC = """
 langcodes is a toolkit for working with and comparing the standardized codes
-for languages, such as 'en' for English and 'zh-Hant' for Traditional Chinese.
+for languages, such as 'en' for English, 'es' for Spanish, and 'zh-Hant' for
+Traditional Chinese. These are BCP 47 language codes, formerly known as ISO
+language codes.
 
 The documentation for langcodes lives in its README file, which you can read
 on GitHub: https://github.com/LuminosoInsight/langcodes
