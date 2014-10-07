479c01737acbbdc7a5004f739c010409f4ad6ffe
Rob Speer <rob@luminoso.com>
2014-10-06 22:03:46 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,16 @@ if sys.version_info[0] < 3:
     )
     sys.exit(1)
 
+
+LONG_DESC = """
+langcodes is a toolkit for working with and comparing the standardized codes
+for languages, such as 'en' for English and 'zh-Hant' for Traditional Chinese.
+
+The documentation for langcodes lives in its README file, which you can read
+on GitHub: https://github.com/LuminosoInsight/langcodes
+"""
+
+
 setup(
     name="langcodes",
     version='1.0',
@@ -21,7 +31,8 @@ setup(
     license="MIT",
     url='http://github.com/LuminosoInsight/langcodes',
     platforms=["any"],
-    description="Labels and compares human languages in a standards-compliant way",
+    description="Labels and compares human languages in a standardized way",
+    long_description=LONG_DESC,
     packages=['langcodes'],
     include_package_data=True,
     classifiers=[
