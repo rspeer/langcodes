0cedf9ca257ebf7250de5d3a63ec33a7d198db58
Rob Speer <rob@luminoso.com>
2018-03-07 17:41:19 -0500

    bump version to 1.4.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ on GitHub: https://github.com/LuminosoInsight/langcodes
 
 setup(
     name="langcodes",
-    version='1.4.0',
+    version='1.4.1',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='rspeer@luminoso.com',
     license="MIT",
