83f432290be8a9668a99d84493508a1d2f68f798
Robyn Speer <rspeer@luminoso.com>
2021-02-05 17:20:29 -0500

    provide population data, if language_data is available

--- langcodes/__init__.py
+++ langcodes/__init__.py
@@ -953,9 +953,98 @@ class Language:
             names['script'] = self.script_name(language, max_distance)
         if self.territory:
             names['territory'] = self.territory_name(language, max_distance)
-        if self.variants:
-            names['variants'] = self.variant_names(language, max_distance)
         return names
+    
+    def speaking_population(self):
+        """
+        Get an estimate of how many people in the world speak this language,
+        derived from CLDR data. Requires that `language_data` is installed.
+        
+        Only the language and territory codes will be considered. If a
+        territory code is included, the population will count only the
+        speakers of the language in that territory.
+
+        Script subtags are disregarded, because it doesn't make sense to ask
+        how many people speak in a particular writing script.
+
+        >>> Language.get('es').speaking_population()
+        487664083
+        >>> Language.get('pt').speaking_population()
+        237135429
+        >>> Language.get('es-BR').speaking_population()
+        76218
+        >>> Language.get('pt-BR').speaking_population()
+        192661560
+        >>> Language.get('vo').speaking_population()
+        0
+        """
+        try:
+            from language_data.population_data import LANGUAGE_SPEAKING_POPULATION
+        except ImportError:
+            print(LANGUAGE_NAME_IMPORT_MESSAGE, file=sys.stdout)
+            raise
+
+        lang = self._filter_attributes(['language', 'territory'])
+        return LANGUAGE_SPEAKING_POPULATION.get(str(lang), 0)
+
+    def writing_population(self):
+        """
+        Get an estimate of how many people in the world read and write
+        this language, derived from CLDR data. Requires that `language_data`
+        is installed.
+
+        For many languages that aren't typically written, this is an
+        overestimate, according to CLDR -- the data often includes people who
+        speak that language but write in a different language.
+        
+        Only the language, script, and territory codes will be considered.
+        If a territory code is included, the population will count only the
+        speakers of the language in that territory.
+
+        >>> all = Language.get('zh').writing_population()
+        >>> all
+        1240326057
+
+        >>> traditional = Language.get('zh-Hant').writing_population()
+        >>> traditional
+        37019589
+
+        >>> simplified = Language.get('zh-Hans').writing_population()
+        >>> all == traditional + simplified
+        True
+
+        >>> Language.get('zh-Hant-HK').writing_population()
+        6439733
+        >>> Language.get('zh-Hans-HK').writing_population()
+        338933
+
+        Note that if you want to get the total Chinese writing population
+        of Hong Kong, you need to avoid normalization that would interpret
+        'zh-HK' as 'zh-Hant-HK'.
+
+        >>> Language.get('zh-HK', normalize=False).writing_population()
+        6778666
+
+        Unknown or unspecified language codes get a population of 0.
+
+        >>> Language.get('xyz').writing_population()
+        0
+
+        >>> Language.get('und').writing_population()
+        0
+        """
+        try:
+            from language_data.population_data import LANGUAGE_WRITING_POPULATION
+        except ImportError:
+            print(LANGUAGE_NAME_IMPORT_MESSAGE, file=sys.stdout)
+            raise
+
+        lang = self._filter_attributes(['language', 'script', 'territory'])
+        if str(lang) in LANGUAGE_WRITING_POPULATION:
+            return LANGUAGE_WRITING_POPULATION[str(lang)]
+        else:
+            lang = lang.simplify_script()
+            return LANGUAGE_WRITING_POPULATION.get(str(lang), 0)
 
     @staticmethod
     def find_name(tagtype: str, name: str, language: {str, 'Language', None}=None):
