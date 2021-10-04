d166c4f675b0bad7a62e73c883129f3fe6f22362
Elia Robyn Speer <elia@explosion.ai>
2021-10-04 17:58:58 -0400

    correct docstring about underscores vs. hyphens

--- langcodes/tag_parser.py
+++ langcodes/tag_parser.py
@@ -141,9 +141,9 @@ SUBTAG_TYPES = [
 
 def normalize_characters(tag):
     """
-    BCP 47 is case-insensitive, and considers underscores equivalent to
-    hyphens. So here we smash tags into lowercase with hyphens, so we can
-    make exact comparisons.
+    BCP 47 is case-insensitive, and CLDR's use of it considers underscores
+    equivalent to hyphens. So here we smash tags into lowercase with hyphens,
+    so we can make exact comparisons.
 
     >>> normalize_characters('en_US')
     'en-us'
